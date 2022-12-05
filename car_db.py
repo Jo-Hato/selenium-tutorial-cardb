284e7faffa7a179ac8ded6d81840039f092f0ec6
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-12-05 17:20:33 +0900

    The code, car_db.py, is now able to scrape some car info. It has completed a test run of 10 pages without a hard-crash. Although, it does not have a way to store those data yet.

--- car_db.py
+++ car_db.py
@@ -1,6 +1,7 @@
 import os
 from time import sleep
 import pandas as pd
+from tabulate import tabulate 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,7 +12,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 # User Parameters
 b_url =  "https://rank.greeco-channel.com/diamtire/?pg=" # base_url
 p_s = 1 # page_start
-p_e = 1 # page_end
+p_e = 10 # page_end
 
 my_options = Options()
 my_options.add_argument("--incognito") # use incognito mode/匿名モードでChromeを使う
@@ -23,18 +24,38 @@ for i in range(p_s, p_e+1):
     url = b_url + str(i)
     print(f"Accessing {url}...")
     driver.get(url)
-    el_tbl = driver.find_element(By.XPATH, "//table[@class='rank_tbl']") # make driver find the table via xpath/ドライバにxpath経由でtblを探させる
-    html_tbl = el_tbl.get_attribute('outerHTML') # get the html as string/htmlの文字列を取得する
-    df = pd.read_html(html_tbl)[0] # cast the html as pd.DataFrame.
-                                # If this raises error "ImportError: lxml not found, please install it", then use "pip3 install lxml"
-                                # htmlをpd.DataFrameにキャストする。
-                                # もし、"ImportError: lxml not found, please install it"というエラーが出たら"pip3 install lxml"を実行する
-    df.to_csv("out.csv")
-    #print(type(df))
-    #print(len(df))
-    #print(df.columns)
-    #driver.save_screenshot(f"ss{str(i)}.png")
-    sleep(1)
+    e_tbls = driver.find_elements(By.XPATH, "//tr[contains(@class,'line27')]/td") # make driver find the table via xpath/ドライバにxpath経由でtblを探させる
+    keys = ["PostDate", "ImgUrl", "Mfr", "MdlCode", "Name", "Grade", "T-OD(mm)", "T-Width", "T-Ratio(%)", "T-[SR]Cnst", "RimR(in)", "GrndClr(mm)", "Archetype"]
+    dicts = []
+    for (i, tbl) in enumerate(e_tbls):
+        txts = tbl.text.split("\n")
+        if (i%5==0):   # 2 "製造元", "型番(省略)"
+            data = dict.fromkeys(keys, None) # Initialize emtpy dict/空の辞書を用意する
+            data["Mfr"] = "".join(txts[:-1])
+        elif (i%5==1): # 1 "画像url"
+            e_c = tbl.find_element(By.XPATH, './*') # e_c = element_child = 要素_子
+            txts = [e_c.get_attribute("src")]
+            data["ImgUrl"] = txts[0]
+        elif (i%5==2): # 4 "投稿日", "車名", "グレード", "型番(Full)"
+            data["PostDate"] = txts[0]
+            data["Name"] = txts[1].replace(" ","") # Remove space/スペースを排除
+            data["Grade"] = txts[2]
+            data["MdlCode"] = None if (txts[3] == "型式不明") else txts[3][1:-1]
+        elif (i%5==3): # 4 "外径”, “タイヤサイズ(mm/ratio/inch)", "地上高", "RPM(100kmph)"
+            data["T-OD(mm)"] = None if (txts[0] == None) else int(txts[0][:-2])
+            tire = txts[1]
+            w, r, sc, rrad = tire[:3], tire[4:6], tire[6:-2], tire[-2:]
+            data["T-Width"] = int(w)
+            data["T-Ratio(%)"] = int(r)
+            data["T-[SR]Cnst"] = sc
+            data["RimR(in)"] = int(rrad)
+            data["GrndClr(mm)"] = None if (txts[2]=="-") else int(txts[2][1:-3])
+        elif (i%5==4): # 4 "エンジン名", "排気量/吸気方式", "ドライブ/ギア数", "車種"
+            data["Archetype"] = txts[3]
+            dicts.append(data)
+    df = pd.DataFrame(dicts)
+    print(df)
+    sleep(3)
 driver.quit()
 
 a = '''
