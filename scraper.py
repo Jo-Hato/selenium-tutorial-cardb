4b69de31c80822461b15022cb20b6c55ea7e9abb
Jo-Hato <jo.shimazaki@cps.akita-pu.ac.jp>
2022-12-07 14:40:31 +0900

    Renamed and deleted files. Notable change is: 'car_db.py' -> 'scraper.py'

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Error Log
+# Log
 e_log = ""
 
 # SQLite3
